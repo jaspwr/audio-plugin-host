9f2fe5eb3b841b6de633f1c01f3f4185fd7477bb
jaspwr <40232406+jaspwr@users.noreply.github.com>
2025-04-10 21:40:32 +1000

    fix: cmake

--- build.rs
+++ build.rs
@@ -1,15 +1,30 @@
-use cmake;
+use cmake::Config;
 
 fn main() {
     println!("cargo:rustc-link-lib=ole32");
 
-    let dst = cmake::build("vst3-wrapper");
+    let dst = Config::new("vst3-wrapper")
+        .build_target("vst3wrapper")
+        .profile("Release")
+        .no_default_flags(true)
+        .build();
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    // println!("cargo:rustc-link-search=native=./vst3-wrapper/host/build/Release");
+    let profile = std::env::var("PROFILE").unwrap();
+    let path = match profile.as_str() {
+        "debug" => dst.join("build").join("Debug"),
+        "release" => dst.join("build").join("Release"),
+        _ => panic!("Unknown profile: {}", profile),
+    };
 
-    println!("cargo:rustc-link-lib=static=VST_SDK");
+    println!("cargo::warning={}", path.display());
+
+    println!("cargo:rustc-link-search=native={}", path.display());
+
+    // println!("cargo:rustc-link-search=native=./vst3-wrapper/build/Release");
+
     println!("cargo:rustc-link-lib=static=vst3wrapper");
+    println!("cargo:rustc-link-lib=static=VST_SDK");
+
     // println!("cargo:rustc-link-lib=stdc++");
     // println!("cargo:rustc-link-lib=static=base");
     // println!("cargo:rustc-link-lib=static=sdk");
@@ -18,4 +33,6 @@ fn main() {
     // println!("cargo:rustc-link-lib=static=pluginterfaces");
     // println!("cargo:rustc-link-arg=-D_GLIBCXX_USE_CXX11_ABI=1");
     // println!("cargo:rustc-link-lib=X11"); // Link the X11 library
+    //
+
 }
